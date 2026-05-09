2163ecbd16eeb1fcf20bddfce27ac81981fbc8f3
aisyahdevantara-hub <aisyahdevantara@gmail.com>
2026-05-09 21:51:12 +0700

    Rapihin Barisannya

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,16 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "db_sekolah") or die("Koneksi gagal: " . mysqli_connect_error());
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_sekolah";
+$port = 3306;
+
+try {
+    $conn = mysqli_connect($host, $user, $pass, $db, $port);
+    $conn->set_charset("utf8mb4");
+} catch (Throwable $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
 ?>
